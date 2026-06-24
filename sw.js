6c559dba9538c6aa313c8f619e28e54c1deb0eb9
daimon-app <teppei.tn.nt@gmail.com>
2026-06-24 22:16:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'daimon-os-v4';
+const CACHE_VERSION = 'daimon-os-v4-3';
 const CACHE_FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(CACHE_FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); self.clients.claim(); });
